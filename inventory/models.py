13b5ccb0d78870da45aa7a1c2cc31e1d0b193fd0
Cesar Martinez <cmatiasmartinezl@gmail.com>
2021-09-06 20:58:47 -0400

    modificacion 06/09

--- inventory/models.py
+++ inventory/models.py
@@ -1,13 +1,11 @@
 from django.db import models
 
-
 class Brand(models.Model):
     name = models.CharField(verbose_name="Nombre", max_length=200)
     created = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
-
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
@@ -24,3 +22,6 @@ class Product(models.Model):
     cost = models.DecimalField(max_digits=10, decimal_places=2)
     price = models.DecimalField(max_digits=10, decimal_places=2)
     created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
